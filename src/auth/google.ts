708975843777c52911d7003c6b292055433ff041
NiteshCodes7 <workwithnites@gmail.com>
2026-04-15 02:42:39 +0530

    OAuth fixing

--- src/auth/google.ts
+++ src/auth/google.ts
@@ -1,24 +1,32 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import axios from 'axios';
 
 export async function getGoogleProfile(code: string) {
+  const params = new URLSearchParams({
+    code,
+    client_id: process.env.GOOGLE_CLIENT_ID!,
+    client_secret: process.env.GOOGLE_CLIENT_SECRET!,
+    redirect_uri: process.env.GOOGLE_CALLBACK_URL!,
+    grant_type: 'authorization_code',
+  });
+
   const { data: tokens } = await axios.post(
-    `https://oauth2.googleapis.com/token`,
+    'https://oauth2.googleapis.com/token',
+    params.toString(),
     {
-      code,
-      client_id: process.env.GOOGLE_CLIENT_ID!,
-      client_secret: process.env.GOOGLE_CLIENT_SECRET!,
-      redirect_uri: process.env.GOOGLE_CALLBACK_URL!,
-      grant_type: 'authorization_code',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
     },
   );
 
   const { data: profile } = await axios.get(
-    `https://www.googleapis.com/oauth2/v2/userinfo`,
+    'https://www.googleapis.com/oauth2/v2/userinfo',
     {
-      headers: { Authorization: `Bearer ${tokens.access_token}` },
+      headers: {
+        Authorization: `Bearer ${tokens.access_token}`,
+      },
     },
   );
 
